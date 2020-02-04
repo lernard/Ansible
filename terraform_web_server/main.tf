4510c842c0f5e3c0225c3683e0b7b623719ad179
lernard <contact@lernard.com>
2020-02-04 10:03:23 +0000

    Added egress to firewall

--- terraform_web_server/main.tf
+++ terraform_web_server/main.tf
@@ -40,6 +40,14 @@ resource "aws_security_group" "web" {
         # Replace this with your own IP address
         cidr_blocks = ["0.0.0.0/0"]
     }
+
+    egress {
+        from_port   = 0
+        to_port     = 65535
+        protocol    = "tcp"
+        # Replace this with your own IP address
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
 
 output "ip_address" {
